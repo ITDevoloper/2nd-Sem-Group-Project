c1b8707108d60a078d3863af34574bce149e34cd
ITDevoloper <junaideenmohomedumar@gmail.com>
2026-05-08 20:24:28 +0530

    Add login form files

---
+++ Librarary System/frmlogin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Librarary_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void lnklblId_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 registerForm = new Form2();
+            registerForm.Show();
+            this.Hide();
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            // get what user typed
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+
+            // check if empty
+            if (user == "" && pass == "")
+            {
+                MessageBox.Show("Please enter username and password.");
+                return;
+            }
+
+            if (user == "")
+            {
+                MessageBox.Show("Please enter username.");
+                return;
+            }
+
+            if (pass == "")
+            {
+                MessageBox.Show("Please enter password.");
+                return;
+            }
+        }
+    }
+}
